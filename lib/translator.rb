0a3019a8e68cc32fdbf89e42846e696e0a4a6c3e
Gerald Tee <93geraldtee@gmail.com>
2019-10-02 16:41:38 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
-# require modules here
-
-def load_library
-  # code goes here
+require 'pry'
+require 'yaml'
+def load_library(emoticons_file)
+  files = YAML.load_file('./lib/emoticons.yml')
+  hash = {:get_meaning => {}, :get_emoticon => {}}
+  files.each do |meaning, emoticons|
+    hash[:get_meaning][emoticons[0]] = meaning
+    hash[:get_emoticon][]
+    binding.pry
+  end
+  hash
 end
 
 def get_japanese_emoticon
