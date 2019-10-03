f13684acd31fca546ff904a7f0b3edcae7fb821a
Gerald Tee <93geraldtee@gmail.com>
2019-10-03 20:53:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_japanese_emoticon(emoticons_file, emoticon)
   emoticon_hash = load_library(emoticons_file)
-  emoticon_hash[:get_emoticon]
+  emoticon_hash[:get_emoticon][emoticon] 
   binding.pry
 end
 
